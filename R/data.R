c81288e4c3147841f25d814350aa8c7896ae4b81
Matt Lawlor <matt.a.lawlor@gmail.com>
2018-06-21 15:48:35 -0400

    data and citation for human

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' human_kinases
+#'
+#' A tibble containing  a catalog of human kinases.
+#'
+#' Derived from an updated version of the kinases described here:
+#'
+#' The Protein Kinase Complement of the Human Genome
+#' G Manning, DB Whyte, R Martinez, T Hunter, S Sudarsanam (2002).
+#' Science 298:1912-1934
+#'
+#' source: http://kinase.com/human/kinome/tables/Kincat_Hsap.08.02.xls
+#'
+#' @format tibble
+"human_kinases"
